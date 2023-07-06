8c4394339300178f010e12c2a4909ae5ce0c790e
zhong <zhong@qq.com>
2023-07-06 19:53:21 +0800

    失败的反向传播

--- test_mat.py
+++ test_mat.py
@@ -3,6 +3,7 @@ import matrix as mat
 import torch.nn as nn
 import torch
 
+
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -10,11 +11,13 @@ class Net(torch.nn.Module):
             torch.nn.Conv2d(1, 1, kernel_size=5),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=2),
+            torch.nn.MaxPool2d(kernel_size=3),
         )
 
     def forward(self, x):
-        x = self.conv1(x)  
-        return x 
+        x = self.conv1(x)
+        x = x.view(-1)
+        return x
 
 
 # mynet = Net()
@@ -23,17 +26,47 @@ class Net(torch.nn.Module):
 # print(y)
 
 batch_size = 64
-learning_rate = 0.01
+learning_rate = 0.05
 momentum = 0.5
 EPOCH = 5
 
-data ={1:mat.a,2:mat.b,3:mat.c,4:mat.d}
+t1 = torch.tensor(
+    [1],
+    dtype=torch.float32,
+)
+t2 = torch.tensor(
+    [2],
+    dtype=torch.float32,
+)
+t3 = torch.tensor(
+    [3],
+    dtype=torch.float32,
+)
+t4 = torch.tensor(
+    [4],
+    dtype=torch.float32,
+)
+data = {t1: mat.a, t2: mat.b, t3: mat.c, t4: mat.d}
 
-for res,input in data.items():
+for res, input in data.items():
     print(res)
 
 model = Net()
- 
-criterion = torch.nn.CrossEntropyLoss()  # 交叉熵损失
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)  # lr学习率，momentum冲量
 
+criterion = torch.nn.CrossEntropyLoss()  # 交叉熵损失
+optimizer = torch.optim.SGD(
+    model.parameters(), lr=learning_rate, momentum=momentum
+)  # lr学习率，momentum冲量
+
+for i in range(0, 1000):
+    for res, input in data.items():
+        optimizer.zero_grad()
+        output = model(input)
+        loss = criterion(output, res)
+
+        loss.backward()
+        optimizer.step()
+
+
+nxx = model(mat.d)
+print(nxx)
