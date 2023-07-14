3290fdc0c6af785040f06a647555e10a8e019f3c
zhong <zhong@qq.com>
2023-07-14 17:35:52 +0800

    sync

--- test_mat1.py
+++ test_mat1.py
@@ -1,4 +1,5 @@
 import matrix as mat
+import random
 
 import torch.nn as nn
 import torch
@@ -7,6 +8,7 @@ import torch
 # 2.0.1+cpu
 
 # print(torch.__version__)
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -49,7 +51,7 @@ class Net(nn.Module):
         x = x.view(-1)
         # x = self.sm(x)
         return x
-    
+
 
 class Net1(torch.nn.Module):
     def __init__(self):
@@ -73,13 +75,15 @@ class Net1(torch.nn.Module):
         batch_size = x.size(0)
         x = self.conv1(x)  # 一层卷积层,一层池化层,一层激活层(图是先卷积后激活再池化，差别不大)
         x = self.conv2(x)  # 再来一次
-        x = x.view(-1)  # flatten 变成全连接网络需要的输入 (batch, 20,4,4) ==> (batch,320), -1 此处自动算出的是320
+        x = x.view(
+            -1
+        )  # flatten 变成全连接网络需要的输入 (batch, 20,4,4) ==> (batch,320), -1 此处自动算出的是320
         x = self.fc(x)
         return x  # 最后输出的是维度为10的，也就是（对应数学符号的0~9）
 
 
 batch_size = 64
-learning_rate = 0.01
+learning_rate = 0.001
 momentum = 0.5
 EPOCH = 5
 
@@ -108,7 +112,9 @@ t4 = torch.tensor(
 # data = {t2: mat.d,t2:mat.test_d}
 # data = [[t2,mat.d],[t2,mat.test_d]]
 
-data = [[t1,mat.bb],[t2,mat.dd],[t3,mat.cc],[t4,mat.aa]]
+data = [[t1, mat.bb], [t2, mat.dd], [t3, mat.cc], [t4, mat.aa]]
+data = [[t1, mat.aa], [t2, mat.bb], [t3, mat.cc], [t4, mat.dd]]
+data = [[t4, mat.aa], [t1, mat.bb], [t2, mat.cc], [t3, mat.dd]]
 # data = [[t1,mat.bb]]
 
 # for res, input in data.items():
@@ -118,20 +124,32 @@ data = [[t1,mat.bb],[t2,mat.dd],[t3,mat.cc],[t4,mat.aa]]
 model = Net1()
 
 criterion = nn.CrossEntropyLoss()  # 交叉熵损失
-optimizer = torch.optim.SGD(
-    model.parameters(), lr=learning_rate
-)  # lr学习率，momentum冲量
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  # lr学习率，momentum冲量
 
-for i in range(0, mat.bas):
-    for val in data:
-        optimizer.zero_grad()
-        output = model(val[1][i])
-        loss = criterion(output, val[0])
+# for i in range(0, mat.bas):
+#     for val in data:
+#         optimizer.zero_grad()
+#         output = model(val[1][i])
+#         loss = criterion(output, val[0])
 
-        print(loss)
-        loss.backward()
-        optimizer.step()
+#         print(loss)
+#         loss.backward()
+#         optimizer.step()
 
+
+for nnx in range(0, 10000):
+    i = random.randint(0, mat.bas-1)
+    index = random.randint(0, 3)
+    val = data[index]
+    optimizer.zero_grad()
+    output = model(val[1][i])
+    loss = criterion(output, val[0])
+
+    if nnx % 100 == 0:
+        print(loss,i,index)
+
+    loss.backward()
+    optimizer.step()
 
 # for param in model.parameters():
 #     print(param)
